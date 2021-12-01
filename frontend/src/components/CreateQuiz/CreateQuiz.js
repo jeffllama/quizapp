7f17dd02f73a0c7a491cdfde43e2dda7d1622255
ChristianEsperanza <53631340+ChristianEsperanza@users.noreply.github.com>
2021-12-01 07:14:41 -0800

    POst

--- frontend/src/components/CreateQuiz/CreateQuiz.js
+++ frontend/src/components/CreateQuiz/CreateQuiz.js
@@ -4,41 +4,44 @@ import { useNavigate } from "react-router-dom";
 function CreateQuiz() {
     const defaultAnswers = { 0: "", 1: "", 2: "", 3: ""}
     const navigate = useNavigate();
-    const [questionValue, setQuestionValue] = useState("")
+    const [questionValues, setQuestionValues] = useState("")
     const [answerValues, setAnswerValues] = useState(["", "", "", ""])
-    const answers = [0,1,2,3]
     const [correctAnswer, setCorrectAnswer] = useState()
+
+    const [questions, setQuestions] = useState({0: "", 1: "", 2: "", 3: "", 4: ""})
+
+    // Amount of questions and answers to be added for a quiz
+    const answers = [0,1,2,3]
+    const questionAmount = [0, 1, 2, 3, 4]
 
     const handleQuestionChange = (event) => {
         const question = event.target.value;
-        setQuestionValue(question)
+        setQuestionValues(question)
+
+        let id = event.target.id;
+        setQuestions({...questions, [id]: question})
+        console.log("Current questions:", questions)
     }
 
+    const handleAnswerChange = (event) => {
+        const id = event.target.id;
+        const answer = event.target.value;
+
+    }
+
+    const handleCorrectAnswerChange = (qNumber) => {
+        // console.log("event", event.target)
+        console.log(qNumber)
+
+    }
+    
     const handleSubmit = (event) => {
         event.preventDefault();
         // Create quiz object and send to back end
     }
 
-    const handleAnswerChange = (event) => {
-        console.log("event", event.target)
-        console.log("event", event.target.id)
-        const { id, value } = event.target
-
-        console.log(answerValues)
-
-        setAnswerValues()
-
-        console.log(answerValues)
-
-
-    }
-
-    const handleCorrectAnswerChange = (event) => {
-
-    }
-
     const check = () => {
-        console.log(questionValue)
+        console.log(questionValues)
     }
 
 
@@ -54,37 +57,49 @@ function CreateQuiz() {
                         </tr>
                     </thead>
                     <tbody>
-                            <tr>
-                                <td>
-                                    <input type="text" name="questionText" placeholder="Enter a question..." onChange={handleQuestionChange}></input>
-                                </td>
-                            </tr>
+                        {questionAmount.map((qNumber) => {
+                            return (
+                            <div>
+                                <tr>
+                                    <td>
+                                        <input id={qNumber} type="text" name="questionText" placeholder="Enter a question..." onChange={handleQuestionChange}></input>
+                                    </td>
 
-                            {answers.map((answer) => {
-                                return (
-                                    <tr>
-                                        <td>
-                                            <input id={answer} type="text" name="answer" placeholder="Enter an answer" onChange={handleAnswerChange} ></input><div/>
-                                        </td>
-                                        <td>
-                                            <input id={answer} type="radio" name="isAnswer" onChange={handleAnswerChange}></input>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                            <button onClick={check}>check</button>
+                                    {answers.map((answer) => {
+                                        return (
+                                            <tr>
+                                                <td>
+                                                    <input id={answer} type="text" name={`answer${qNumber}`} placeholder="Enter an answer" onChange={handleAnswerChange} ></input><div/>
+                                                </td>
+                                                <td>
+                                                    <input id={answer} type="radio" name={`isAnswer${qNumber}`} onChange={handleCorrectAnswerChange(qNumber)}></input>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tr>
+                                <tr></tr>
+                            </div>
+
+                            )
+                        })}
 
 
                     </tbody>
                 </table>
+                
                 {/* <form onSubmit={handleSubmit}>
-                    <input type="text" name="questionText" placeholder="Enter your question..."></input><div/>
+                    <input type="text" name="nnText" placeholder="Enter your question..."></input><div/>
                     <input type="text" name="answerText1" placeholder="Enter an answer"></input><div/>
                     <input type="text" name="answerText2" placeholder="Enter an answer"></input><div/>
                     <input type="text" name="answerText3" placeholder="Enter an answer"></input><div/>
                     <input type="text" name="answerText4" placeholder="Enter an answer"></input><div/>
                     <input type="submit" />
                 </form> */}
+            </div> 
+
+            <div id="submitContainer">
+                <button onClick={handleSubmit}>Submit</button>
             </div>
 
             <div id="homeBtn">
